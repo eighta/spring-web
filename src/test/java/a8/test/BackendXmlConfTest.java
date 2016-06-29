2c57c3d3ea124e1a06997e9655bd01742fb0382a
Javier Larios <ochoalarios@gmail.com>
2016-06-29 17:45:27 -0500

    Se crea test para la configuracion en XML del backend

---
+++ src/test/java/a8/test/BackendXmlConfTest.java
@@ -0,0 +1,13 @@
+package a8.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BackendXmlConfTest {
+
+	@Test
+	public void dummyTest(){
+		assertTrue(true);
+	}
+}
