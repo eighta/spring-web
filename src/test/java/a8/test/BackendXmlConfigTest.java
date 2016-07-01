dbd3b8e3fc2d3ab5b19ff14f9e78d0ad6b3a3d6c
eighta <ochoalarios@gmail.com>
2016-06-30 23:18:24 -0500

    WARNING OFF

--- src/test/java/a8/test/BackendXmlConfigTest.java
+++ src/test/java/a8/test/BackendXmlConfigTest.java
@@ -1,8 +1,8 @@
 package a8.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
