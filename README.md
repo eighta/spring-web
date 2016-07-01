698fc0fdc06bc0bf6295f45f4b341da4d9605fbc
Javier Larios <ochoalarios@gmail.com>
2016-07-01 15:29:45 -0500

    Se define un color para la terminal

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # spring-web
 Proyecto en donde se plasmaran los conocimientos adquiridos en el spring-web
 (y repaso del Spring Framework)
+
+#install and setup Grep Console plugin 
+
+#Console Colors
+#393939	background
+#C7C7C7	simple text
+
+#logcColors
+#56C1E9	DEBUG
+#A9D40D	INFO
+#FAC848	WARNING
+#F55C5A	ERROR
+#9878D5	ASSERT
+
+#Font	MONACO
--- src/test/java/a8/test/plain/Main.java
+++ src/test/java/a8/test/plain/Main.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import ch.qos.logback.classic.Level;
+
 public class Main {
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -14,8 +16,44 @@ public class Main {
 	public static void main(String[] args) {
 		testClassPathXmlApplicationContext();
 		retrieveOsName();
+		testLogLevel();
 	}
 	
+	private static void testLogLevel() {
+		
+System.out.println("PRINTING ALL LEVELS");
+		
+		logger.trace("1-TRACE MSG");
+		logger.debug("2-DEBUG MSG");
+		logger.info("3-INFO MSG");
+		logger.warn("4-WARNING MSG");
+		logger.error("5-ERROR MSG");
+
+System.out.println("END LEVELS");
+
+		//TRACE < DEBUG < INFO <  WARN < ERROR
+		ch.qos.logback.classic.Logger realLogger = (ch.qos.logback.classic.Logger) logger;
+		
+		realLogger.setLevel(Level.WARN);
+		realLogger.warn("WARNING MESSAGE");
+		realLogger.info	("no imprime INFO por tener effective level WARN (por ser INFO < WARN)");
+		realLogger.error("SI imprime ERROR por tener effective level WARN (por ser WARN < ERROR)");
+		
+		//Appenders
+		
+		//adding a appender
+		//realLogger.addAppender(...);
+		
+		//loggin conditional
+		if(logger.isDebugEnabled()) {
+			System.out.println("DEBUG ENABLED");
+			logger.debug("Message 4 debug LEVEL");
+		}else{
+			System.out.println("DEBUG DISABLED");
+		}
+		
+	}
+
 	public static void retrieveOsName(){
 		String osName = System.getProperty("os.name");
 		logger.info(osName);
