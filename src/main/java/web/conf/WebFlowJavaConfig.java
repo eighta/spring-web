c52929bcb9369a165cb609a8b0b457b7127976e3
Javier Larios <ochoalarios@gmail.com>
2016-09-30 17:25:34 -0500

    SE ACABO SEPTIEMBRE

--- src/main/java/web/conf/WebFlowJavaConfig.java
+++ src/main/java/web/conf/WebFlowJavaConfig.java
@@ -1,14 +1,11 @@
 package web.conf;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.binding.convert.service.DefaultConversionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.Formatter;
-import org.springframework.format.support.FormattingConversionServiceFactoryBean;
 import org.springframework.webflow.config.AbstractFlowConfiguration;
+import org.springframework.webflow.config.FlowBuilderServicesBuilder;
+import org.springframework.webflow.config.FlowDefinitionRegistryBuilder;
+import org.springframework.webflow.config.FlowExecutorBuilder;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.executor.FlowExecutor;
@@ -17,8 +14,13 @@ import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
 import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 
 @Configuration
-public class WebFlowJavaConfig extends AbstractFlowConfiguration {
+public class WebFlowJavaConfig 
+extends AbstractFlowConfiguration 
+{
 
+	//==================
+	//FlowHandlerMapping
+	//==================
 	@Bean
 	public FlowHandlerMapping flowHandlerMapping() {
 		FlowHandlerMapping handlerMapping = new FlowHandlerMapping();
@@ -26,7 +28,10 @@ public class WebFlowJavaConfig extends AbstractFlowConfiguration {
 		handlerMapping.setFlowRegistry(this.flowRegistry());
 		return handlerMapping;
 	}
-
+	
+	//==================
+	//FlowHandlerAdapter
+	//==================
 	@Bean
 	public FlowHandlerAdapter flowHandlerAdapter() {
 		FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
@@ -34,54 +39,70 @@ public class WebFlowJavaConfig extends AbstractFlowConfiguration {
 		handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
 		return handlerAdapter;
 	}
-
-	@Bean
-	public FlowExecutor flowExecutor() {
-		return getFlowExecutorBuilder(this.flowRegistry())
-				
-				// apply the listener for all flow definitions
-				// .addFlowExecutionListener(new AuditFlowExecutorListener(), "*")
-				
-				.setMaxFlowExecutions(5)
-				.setMaxFlowExecutionSnapshots(30)
-				.build();
-	}
-
-	@Bean
-	public FlowDefinitionRegistry flowRegistry() {
-		return getFlowDefinitionRegistryBuilder(flowBuilderServices()).setBasePath("/WEB-INF/flows")
-				.addFlowLocationPattern("/**/*-flow.xml").build();
-	}
-
-	@Bean
-	public FlowBuilderServices flowBuilderServices() {
-		return getFlowBuilderServicesBuilder().setViewFactoryCreator(mvcViewFactoryCreator())
-				// .setValidator(this.mvcConfig.validator())
-				.setConversionService(conversionService())
-				.setDevelopmentMode(true).build();
-	}
-
-	@Bean
+	
+	//=========================
+	//OTHERs WebFlow Components
+	//=========================
+	
+/*	@Bean
 	public MvcViewFactoryCreator mvcViewFactoryCreator() {
 		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
-		// factoryCreator.setViewResolvers(Arrays.<ViewResolver>asList(this.mvcConfig.tilesViewResolver()));
-		factoryCreator.setUseSpringBeanBinding(true);
+		//factoryCreator.setViewResolvers(Arrays.<ViewResolver>asList(this.mvcConfig.tilesViewResolver()));
+		//factoryCreator.setUseSpringBeanBinding(true);
 		return factoryCreator;
 	}
-
+*/	
+	
 	@Bean
-	public DefaultConversionService conversionService() {
-		return new DefaultConversionService(conversionServiceFactoryBean().getObject());
+	public FlowBuilderServices flowBuilderServices() {
+		
+		FlowBuilderServicesBuilder flowBuilderServicesBuilder = super.getFlowBuilderServicesBuilder();
+		//flowBuilderServicesBuilder.setViewFactoryCreator(this.mvcViewFactoryCreator());
+		//flowBuilderServicesBuilder.setValidator(this.mvcConfig.validator())
+		//flowBuilderServicesBuilder.setConversionService(conversionService())
+		//flowBuilderServicesBuilder.setDevelopmentMode(true)
+		
+		return flowBuilderServicesBuilder.build();
+	}
+	
+	@Bean
+	public FlowDefinitionRegistry flowRegistry() {
+		
+		FlowDefinitionRegistryBuilder flowDefinitionRegistryBuilder = super.getFlowDefinitionRegistryBuilder(this.flowBuilderServices());
+		flowDefinitionRegistryBuilder.setBasePath("/WEB-INF/flows");
+		flowDefinitionRegistryBuilder.addFlowLocationPattern("/**/*-flow.xml");
+		
+		return flowDefinitionRegistryBuilder.build();
+	}
+	
+	@Bean
+	public FlowExecutor flowExecutor() {
+		
+		FlowExecutorBuilder flowExecutorBuilder = super.getFlowExecutorBuilder(this.flowRegistry());
+		
+		// apply the listener for all flow definitions
+		//flowExecutorBuilder.addFlowExecutionListener(new AuditFlowExecutorListener(), "*")
+		
+		flowExecutorBuilder.setMaxFlowExecutions(5);
+		flowExecutorBuilder.setMaxFlowExecutionSnapshots(30);
+		
+		return flowExecutorBuilder.build();
 	}
 
-	@Bean
-	public FormattingConversionServiceFactoryBean conversionServiceFactoryBean() {
-		FormattingConversionServiceFactoryBean fcs = new FormattingConversionServiceFactoryBean();
-		Set<Formatter> fmts = new HashSet<>();
-		// fmts.add(this.mvcConfig.dateFormatter());
-		// fmts.add(this.mvcConfig.hospitalFormatter());
-		fcs.setFormatters(fmts);
-		return fcs;
-	}
+//
+//	@Bean
+//	public DefaultConversionService conversionService() {
+//		return new DefaultConversionService(conversionServiceFactoryBean().getObject());
+//	}
+//
+//	@Bean
+//	public FormattingConversionServiceFactoryBean conversionServiceFactoryBean() {
+//		FormattingConversionServiceFactoryBean fcs = new FormattingConversionServiceFactoryBean();
+//		Set<Formatter> fmts = new HashSet<>();
+//		// fmts.add(this.mvcConfig.dateFormatter());
+//		// fmts.add(this.mvcConfig.hospitalFormatter());
+//		fcs.setFormatters(fmts);
+//		return fcs;
+//	}
 
 }
