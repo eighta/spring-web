2f1bb75de81341e98a2911e28ef01e4c226709f3
Javier Larios <ochoalarios@gmail.com>
2016-10-24 15:44:57 -0500

    eight days left

--- src/test/java/a8/test/webflow/NewPersonFlowTest.java
+++ src/test/java/a8/test/webflow/NewPersonFlowTest.java
@@ -1,14 +1,23 @@
 package a8.test.webflow;
 
-import org.springframework.context.annotation.Bean;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.binding.collection.SharedMap;
+import org.springframework.binding.collection.SharedMapDecorator;
 import org.springframework.webflow.config.FlowDefinitionResource;
 import org.springframework.webflow.config.FlowDefinitionResourceFactory;
+import org.springframework.webflow.core.collection.LocalAttributeMap;
+import org.springframework.webflow.core.collection.LocalSharedAttributeMap;
+import org.springframework.webflow.core.collection.SharedAttributeMap;
+import org.springframework.webflow.execution.ActionExecutor;
 import org.springframework.webflow.test.MockExternalContext;
 import org.springframework.webflow.test.MockFlowBuilderContext;
 import org.springframework.webflow.test.execution.AbstractXmlFlowExecutionTests;
 
 import a8.data.Person;
 import a8.services.InterviewFactory;
+import a8.validators.PersonValidator;
 import web.listeners.WebFlowListener;
 
 public class NewPersonFlowTest extends AbstractXmlFlowExecutionTests {
@@ -28,22 +37,63 @@ public class NewPersonFlowTest extends AbstractXmlFlowExecutionTests {
 	protected void configureFlowBuilderContext(
 			MockFlowBuilderContext builderContext) {
 		
-		builderContext.registerBean("person4Grammy", new Person());
+//		builderContext.registerBean("person4Grammy", new Person());
 		builderContext.registerBean("interviewFactory", new InterviewFactory());
 		builderContext.registerBean("theFlowListener", new WebFlowListener());
 		
+		
+		builderContext.registerBean("personValidator", new PersonValidator());
+		builderContext.registerBean("newPerson", new Person() );
+		
+		
+		
 	}
+
+	//Al ejecutar esta clase con JUnit, el motor indetificará 
+	//aquellos metodos que empiecen por la palabra "test"
+	//para luego ejecutarlos
+	
+	public void testStart(){
+		
+		startFlow(new MockExternalContext());
+		assertCurrentStateEquals("enterMainPersonInfo");
+	}
+	
+	public void testEnterLocationInfoProceed(){
+		
+		setCurrentState("enterMainPersonInfo");
+		MockExternalContext context = new MockExternalContext();
+		context.setEventId("next");
+		
+		resumeFlow(context);
+		assertCurrentStateEquals("enterLocationPersonInfo");
+		
+	}
+	
+	public void testEndState(){
+		
+		startFlow(new MockExternalContext());
+		
+		MockExternalContext context = new MockExternalContext();
+		context.setEventId("cancel");
+		
+		resumeFlow(context);
+		assertFlowExecutionEnded();
+		assertFlowExecutionOutcomeEquals("cancelled");
+		//assertTrue(context.getExternalRedirectRequested());
+	}
+	
 	
 //	private static final String ENTER_PERSON_INFO = "enterPersonInfo";
 //	
-	public void testNewPersonCreation() throws Exception {
+	public void _testNewPersonCreation() throws Exception {
 		MockExternalContext context = new MockExternalContext();
 		startFlow(context);
 		assertCurrentStateEquals("enterMainPersonInfo");
 		assertNotNull(getRequiredFlowAttribute("grammy"));
 	}
 	
-	public void testTransition(){
+	public void _testTransition(){
 		setCurrentState("enterLocationPersonInfo");
 		MockExternalContext context = new MockExternalContext();
 		context.setEventId("next");
@@ -51,9 +101,7 @@ public class NewPersonFlowTest extends AbstractXmlFlowExecutionTests {
 		assertCurrentStateEquals("enterKeyPersonInfo");
 	}
 	
-	//LOS METODOS QUE SE EJECUTARAN
-	//los nombres deben empezar por "test"
-	public void testEndState(){
+	public void _testEndState(){
 		setCurrentState("reviewPersonInfo");
 		//getFlowScope().put("diningForm", diningForm);
 		MockExternalContext externalContext = new MockExternalContext();
@@ -65,16 +113,4 @@ public class NewPersonFlowTest extends AbstractXmlFlowExecutionTests {
 		assertEquals("contextRelative:/s/tasks/webflow/persons/1",
 				externalContext.getExternalRedirectUrl());
 	}
-	
-//	java.lang.IllegalStateException: No active FlowSession to access; this FlowExecution has ended
-//	public void testFinishCreation(){
-//		setCurrentState("reviewPersonInfo");
-//		
-//		MockExternalContext context = new MockExternalContext();
-//		context.setEventId("confirm");
-//		resumeFlow(context);
-//		
-//		assertCurrentStateEquals("end");
-//	}
-
 }
